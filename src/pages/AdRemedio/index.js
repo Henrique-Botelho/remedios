47371206fe4852d7d860afa2657fc17db94e71c1
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-04-07 21:38:17 -0300

    Estilização da página Adicionar Remédio

--- src/pages/AdRemedio/index.js
+++ src/pages/AdRemedio/index.js
@@ -7,7 +7,9 @@ import {
   StyleSheet,
   TouchableOpacity,
   ActivityIndicator,
+  ImageBackground
 } from "react-native";
+import Footer from "../../components/Footer";
 
 import { MainContext } from "../../context/mainContext";
 
@@ -21,55 +23,116 @@ export default function AdRemedio({ navigation }) {
   const [adicionando, setAdicionando] = useState(false);
 
   return (
-    <SafeAreaView style={styles.page}>
-      <TextInput
-        maxLength={20}
-        placeholder="Nome do remédio"
-        style={styles.inputs}
-        onChangeText={(newText) => setNome(newText)}
-        value={nome}
-      />
-      <TextInput
-        placeholder="Quantidade da caixa"
-        style={styles.inputs}
-        onChangeText={(newText) => setQuantCaixa(newText)}
-        value={quantCaixa}
-        keyboardType="numeric"
-      />
-      <TextInput
-        placeholder="Quantidade por dia"
-        style={styles.inputs}
-        onChangeText={(newText) => setQuantDia(newText)}
-        value={quantDia}
-        keyboardType="numeric"
-      />
-      <TouchableOpacity
-        onPress={() => {
-          setAdicionando(true);
-          adicionarRemedio(nome.trim(), quantCaixa, quantDia).then(() => {
-            pegaRemedios().then(() => {
-              setAdicionando(false);
-              navigation.navigate("Remedios");
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.caixaImg}>
+        <ImageBackground style={styles.imgAdRem} source={require('../../../assets/adrem.jpg')}>
+          <View style={styles.caixaTitPagina}>
+            <Text style={styles.titPagina}>Adicionar Remédio</Text>
+          </View>
+        </ImageBackground>
+      </View>
+      <View style={styles.page}>
+
+        <TextInput
+          maxLength={20}
+          placeholder="Nome do remédio"
+          style={styles.inputs}
+          placeholderTextColor='#66AF91'
+          onChangeText={(newText) => setNome(newText)}
+          value={nome}
+          />
+        <TextInput
+          placeholder="Quantidade da caixa"
+          style={styles.inputs}
+          placeholderTextColor='#66AF91'
+          onChangeText={(newText) => setQuantCaixa(newText)}
+          value={quantCaixa}
+          keyboardType="numeric"
+          />
+        <TextInput
+          placeholder="Quantidade por dia"
+          style={styles.inputs}
+          placeholderTextColor='#66AF91'
+          onChangeText={(newText) => setQuantDia(newText)}
+          value={quantDia}
+          keyboardType="numeric"
+        />
+        <TouchableOpacity
+          style={styles.caixaAd}
+          onPress={() => {
+            setAdicionando(true);
+            adicionarRemedio(nome.trim(), quantCaixa, quantDia).then(() => {
+              pegaRemedios().then(() => {
+                setAdicionando(false);
+                navigation.navigate("Remedios");
+              });
             });
-          });
-        }}
-      >
-        <Text>Adicionar</Text>
-      </TouchableOpacity>
-      {adicionando && <ActivityIndicator size="large" />}
+          }}
+        >
+          <Text style={styles.adicionar}>Adicionar</Text>
+        </TouchableOpacity>
+        {adicionando && <ActivityIndicator size="large" />}
+      </View>
+      <Footer />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
+  safeArea: {
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#D0F2E7",
+    width: "100%",
+  },
+  caixaImg: {
+    height: '30%',
+    width: '100%'
+  },
+  imgAdRem: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  caixaTitPagina: {
+    height: 50,
+    backgroundColor: '#ADE0DB',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopLeftRadius: 20,
+    width: '75%'
+  },
+  titPagina: {
+    fontSize: 25,
+    color: '#66AF91'
+  },
+  page: {
+    height: "65%",
+    width: "90%",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "#D0F2E7",
+    paddingTop: 40,
   },
   inputs: {
-    borderWidth: 3,
-    width: "50%",
-    textAlign: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: '#A2CA8E',
+    width: "75%",
+    textAlign: "left",
+    fontSize: 20,
+    color: '#66AF91'
   },
+  caixaAd: {
+    backgroundColor: '#66AF91',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    borderRadius: 20
+  },
+  adicionar: {
+    color: '#EBF2F2',
+    fontSize: 20
+  }
 });
--- src/routes.js
+++ src/routes.js
@@ -15,22 +15,36 @@ export default function Routes() {
     <NavigationContainer>
       <MainProvider>
         <Stack.Navigator>
-          <Stack.Screen name="Remedios" component={Remedios} options={{
-            title: 'Remédios',
-            headerStyle: {
-              backgroundColor: '#ADE0DB',
-              height: 90
-            },
-            headerTitleAlign: 'center',
-            headerTintColor: '#66AF91',
-            headerTitleStyle: {
-              fontSize: 30
-            }
-          }} />
-          <Stack.Screen name="AdRemedio" component={AdRemedio} />
-          <Stack.Screen name="EditaRemedio" component={EditaRemedio} />
+          <Stack.Screen
+            name="Remedios"
+            component={Remedios}
+            options={headerOptions}
+          />
+          <Stack.Screen
+            name="AdRemedio"
+            component={AdRemedio}
+            options={headerOptions}
+          />
+          <Stack.Screen
+            name="EditaRemedio"
+            component={EditaRemedio}
+            options={headerOptions}
+          />
         </Stack.Navigator>
       </MainProvider>
     </NavigationContainer>
   );
 }
+
+const headerOptions = {
+  title: "Remédios",
+  headerStyle: {
+    backgroundColor: "#ADE0DB",
+    height: 90,
+  },
+  headerTitleAlign: "center",
+  headerTintColor: "#66AF91",
+  headerTitleStyle: {
+    fontSize: 30,
+  },
+};
